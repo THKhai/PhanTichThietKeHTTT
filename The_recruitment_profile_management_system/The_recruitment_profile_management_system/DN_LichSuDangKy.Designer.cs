0cd0a5654a35b173f3b9d06e96fa4e804699cea8
BCKhang <138310391+BCKhang@users.noreply.github.com>
2024-05-10 22:00:43 +0700

    khang

--- The_recruitment_profile_management_system/The_recruitment_profile_management_system/DN_LichSuDangKy.Designer.cs
+++ The_recruitment_profile_management_system/The_recruitment_profile_management_system/DN_LichSuDangKy.Designer.cs
@@ -31,17 +31,24 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(35, 87);
+            this.dataGridView1.Location = new System.Drawing.Point(35, 72);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(728, 284);
+            this.dataGridView1.Size = new System.Drawing.Size(728, 151);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -59,17 +66,87 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(266, 32);
+            this.label1.Location = new System.Drawing.Point(266, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(253, 39);
             this.label1.TabIndex = 21;
             this.label1.Text = "Lịch sử đăng ký";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(35, 256);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(728, 134);
+            this.richTextBox1.TabIndex = 22;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(346, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 18);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Hóa đơn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(417, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(417, 316);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(417, 343);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "label7";
             // 
             // DN_LichSuDangKy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -87,5 +164,12 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
--- The_recruitment_profile_management_system/The_recruitment_profile_management_system/DN_LichSuDangKy.cs
+++ The_recruitment_profile_management_system/The_recruitment_profile_management_system/DN_LichSuDangKy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using Oracle.DataAccess.Client;
 using Oracle.ManagedDataAccess.Client;
 
 namespace The_recruitment_profile_management_system
@@ -21,6 +23,11 @@ namespace The_recruitment_profile_management_system
         }
         private void DN_LichSuDangKy_Load(object sender, EventArgs e)
         {
+            label3.Hide();
+            label4.Hide(); 
+            label5.Hide();
+            label6.Hide();
+            label7.Hide();
             try
             {
                 using (OracleConnection con = new OracleConnection(connectString))
@@ -45,7 +52,62 @@ namespace The_recruitment_profile_management_system
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            if(e.RowIndex >= 0 && e.ColumnIndex >= 0)
+    {
+                object cellValue = dataGridView1.Rows[e.RowIndex].Cells[0].Value;
+                try
+                {
+                    using (OracleConnection conn = new OracleConnection(connectString))
+                    {
+                        string query = "SELECT * FROM sys.v_DNHoaDon where MaTTDT = '" + cellValue.ToString() + "'";
+                        OracleCommand cmd = new OracleCommand(query, conn);
 
+                        // Add parameter for YourVariable
+                        //cmd.Parameters.Add(":YourVariable", OracleDbType.Varchar2).Value = yourVariable;
+
+                        // Open the connection
+                        conn.Open();
+
+                        // Execute the command and retrieve the result into a OracleDataReader
+                        using (OracleDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string column1Value = reader.GetString(0);
+                                string column2Value = reader.GetString(1);
+                                string column3Value = reader.GetString(2);
+                                string column4Value = reader.GetString(3);
+                                string column5Value = reader.GetString(4);
+
+                                label3.Text = "Mã số thuế: " + column1Value;
+                                label4.Text = "Mã TTDT: " + column2Value;
+                                label5.Text = "Hình thức đăng tuyển: " + column3Value;
+                                label6.Text = "Hình thức thanh toán: " + column4Value;
+                                label7.Text = "Tổng tiền: " + column5Value + "đ";
+
+                                label3.Show();
+                                label4.Show();
+                                label5.Show();
+                                label6.Show();
+                                label7.Show();
+                            }
+                            else
+                            {
+                                label3.Hide();
+                                label4.Hide();
+                                label5.Hide();
+                                label6.Hide();
+                                label7.Hide();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                //MessageBox.Show("Clicked cell value: " + cellValue.ToString());
+            }
         }
     }
 }
