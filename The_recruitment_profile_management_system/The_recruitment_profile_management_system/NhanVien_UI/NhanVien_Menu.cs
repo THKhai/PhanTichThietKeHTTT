71c3489a7fdda3ae5d2cda9579bb20d98fac31a6
Từ Toàn <102405147+Toantv1234@users.noreply.github.com>
2024-05-10 21:37:10 +0700

    update NhanVien UI

---
+++ The_recruitment_profile_management_system/The_recruitment_profile_management_system/NhanVien_UI/NhanVien_Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_recruitment_profile_management_system.NhanVien_UI
+{
+    public partial class NhanVien_Menu : Form
+    {
+        string connect = null;
+        public NhanVien_Menu(string Connect)
+        {
+            InitializeComponent();
+            connect = Connect;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThongKeDoanhNghiepSapHetHan tk = new ThongKeDoanhNghiepSapHetHan(connect);
+            tk.ShowDialog();
+            this.Show();
+        }
+    }
+}
---
+++ The_recruitment_profile_management_system/The_recruitment_profile_management_system/NhanVien_UI/ThongKeDoanhNghiepSapHetHan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_recruitment_profile_management_system.NhanVien_UI
+{
+    public partial class ThongKeDoanhNghiepSapHetHan : Form
+    {
+        string connect = null;
+        public ThongKeDoanhNghiepSapHetHan(string conn)
+        {
+            InitializeComponent();
+            connect = conn;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
